b671f2f34ac3c8c54ddb9cc21def42942b62799f
Karmybree <brendamukamih99@gmail.com>
2023-02-16 14:01:01 -0800

    task4

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,39 @@
+#include <stdio.h>
+/**
+
+#include <stdio.h>
+
+/**
+
+#include <stdio.h>
+
+/**
+ * main - Entry Point
+ *
+ * Return: Always 0 (Success)
+ */
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+        puts("\"Programming is like building a multilingual puzzle");
+               
+
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+        puts("\"Programming is like building a multilingual puzzle");
+        return (0);
+
+} 
