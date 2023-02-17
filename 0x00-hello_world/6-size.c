28d52c3dad13b5d7f5028a9738475b476a4c13c0
Karmybree <brendamukamih99@gmail.com>
2023-02-16 18:18:04 -0800

    task6

---
+++ 0x00-hello_world/6-size.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+/**
+ * main - Prints the sie of various typesa based on
+ * the computer it is compiled and run on..
+ * Return: Always 0.
+ */
+
+int main(void)
+
+{
+	printf("sie of a char: %zu byte(s)\n", sizeof(char));
+
+	print("Size of an int: %zu byte(s)\n", sizeof(int));
+	print("Size of a long int: %zu byte(s)\n", sizeof(long int));
+	print("Size of a long long int: %zu byte(s)\n", sizeof(long long int));
+	print("Size of a float %zu byte(s)\n", sizeof(float));
+	return (0);
+}
+
