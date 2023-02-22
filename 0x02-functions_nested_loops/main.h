6acb1830381dfd6680b96a0d1277b96d02e55e0a
Karmybree <brendamukamih99@gmail.com>
2023-02-22 14:31:39 -0800

    task0

---
+++ 0x02-functions_nested_loops/main.h
@@ -0,0 +1,45 @@
+#ifndef MAIN_H
+#ifndef main_h
+#define main_h
+
+void print_alphabet(void);
+void print _alphabet_x10(void);
+int _islower(int c);
+int _isalpha(int c);
+int print_sign(int n);
+int _abs(int);
+int print_last_digit(int);
+void jack_bauer(void);
+int add(int, int);
+void print_to_98(int n)
+
+
+#ifndef main_h
+#define main_h
+
+void print_alphabet(void);
+void print _alphabet_x10(void);
+int _islower(int c);
+int _isalpha(int c);
+int print_sign(int n);
+int _abs(int);
+int print_last_digit(int);
+void jack_bauer(void);
+int add(int, int);
+void print_to_98(int n)
+
+#ifndef main_h
+#define main_h
+
+void print_alphabet(void);
+void print _alphabet_x10(void);
+int _islower(int c);
+int _isalpha(int c);
+int print_sign(int n);
+int _abs(int);
+int print_last_digit(int);
+void jack_bauer(void);
+int add(int, int);
+void print_to_98(int n);
+
+#endif
