6bf481a8a86dea495bfbc320c954bec35de1b0ee
Karmybree <brendamukamih99@gmail.com>
2023-02-23 07:16:08 -0800

    task1

---
+++ 0x03-debugging/1-main.c
@@ -0,0 +1,24 @@
+[16:45, 23/02/2023] Justin Alx: #include <stdio.h>
+
+/**
+ * main - causes an infinite loop
+ * Return: 0
+ */
+
+int main(void)
+{
+	int i;
+
+	printf("Infinite loop incoming :(\n");
+
+	i = 0;
+	/**
+	 * while (i < 10)
+	 * {
+	 * putchar(i);
+	 * }
+	 */
+	printf("Infinite loop avoided! \\o/\n");
+
+	return (0);
+}
